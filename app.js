95d6374b77bdfe402acc0ac32d5f437fa219dd02
mkoeplin <55862880+mkoeplin@users.noreply.github.com>
2020-10-05 13:41:17 -0500

    Add error functions

--- app.js
+++ app.js
@@ -78,3 +78,18 @@ app.get('/logout', function (req, res, next) {
     req.session.destroy()
     res.redirect('/login')
 })
+
+app.use(function (req, res) {
+    res.status(404)
+    res.render('404')
+})
+
+app.use(function (err, req, res, next) {
+    console.log(err.stack)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(app.get("port"), function () {
+    console.log("Express started on http://flipX.engr.oregonstate.edu:" + app.get("port") + " press Ctrl-C to terminate.")
+})
