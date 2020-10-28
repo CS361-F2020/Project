e14a2eaca64dc77c5d3dd835f5f7a6d512e463a0
Andrew Jung <53200592+jung8@users.noreply.github.com>
2020-10-28 00:03:40 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");	
 var pool = mysql.createPool({	
     host: "classmysql.engr.oregonstate.edu",	
-    user: "<onid>_cs361",	
+    user: "cs361_<onid>",	
     password: "<password>",	
-    database: "<onid>_cs361",	
+    database: "cs361_<onid>",	
     dateStrings: 'true',	
     typeCast: function castField(field, useDefaultTypeCasting) {	
         if (field.type === "BIT" && field.length === 1) {	
