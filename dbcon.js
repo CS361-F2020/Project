282b42c82ac2c04d3322ecbc6504d0c4dccd2fdf
Andrew Jung <jungan@flip1.engr.oregonstate.edu>
2020-10-29 14:52:24 -0700

    mylibrary

--- dbcon.js
+++ dbcon.js
@@ -1,17 +1,17 @@
-var mysql = require("mysql");	
-var pool = mysql.createPool({	
-    host: "classmysql.engr.oregonstate.edu",	
-    user: "cs361_<onid>",	
-    password: "<password>",	
-    database: "cs361_<onid>",	
-    dateStrings: 'true',	
-    typeCast: function castField(field, useDefaultTypeCasting) {	
-        if (field.type === "BIT" && field.length === 1) {	
-            var bytes = field.buffer();	
-            return (bytes[0] === 1);	
-        }	
-        return useDefaultTypeCasting();	
-    }	
-});	
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    host: "classmysql.engr.oregonstate.edu",
+    user: "cs361_jungan",
+    password: "7505",
+    database: "cs361_jungan",
+    dateStrings: 'true',
+    typeCast: function castField(field, useDefaultTypeCasting) {
+        if (field.type === "BIT" && field.length === 1) {
+            var bytes = field.buffer();
+            return (bytes[0] === 1);
+        }
+        return useDefaultTypeCasting();
+    }
+});
 
 module.exports.pool = pool; 
