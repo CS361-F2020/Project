2afff2a81498893a03b32a2af972b91c31c30b93
Kylee Hale <halekyl@flip2.engr.oregonstate.edu>
2020-11-15 10:02:59 -0800

    removed dbcon credentials

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
     host: "classmysql.engr.oregonstate.edu",
-    user: "cs361_halekyl",
-    password: "2088",
-    database: "cs361_halekyl",
+    user: "cs361_<onid>",
+    password: "<password>",
+    database: "cs361_<onid>",
     dateStrings: 'true',
     typeCast: function castField(field, useDefaultTypeCasting) {
         if (field.type === "BIT" && field.length === 1) {
