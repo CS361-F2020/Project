e4ead8c52a37d7c358cb0a408e332f4e3a50ee6e
glennfrutiz <45412458+glennfrutiz@users.noreply.github.com>
2020-11-16 21:01:05 -0600

    Update common.js (#35)
    * Update common.js
    
    * Update common.js
    
    i think this is working? I am getting back a value for total points based on what I have in transactions. 
    
    I am unsure if a value will be available to you when you call this though. If you still think this requires a call back function please let me know. I think i got it working with a callback function as well but i opted for this as it is much cleaner.
    
    * Update common.js
    
    removed type

--- common.js
+++ common.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer')
+const db = require('./dbcon.js');
 
 var transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -18,7 +19,31 @@ function isAuthenticated(req, res, next) {
     }
 }
 
+
+function getPendingPoints(userId){
+    var outgoing = `SELECT SUM(u.points) AS totalPoints
+    FROM 
+    (SELECT (SUM(Transactions.pointCost) * -1) AS points 
+    FROM Transactions 
+    WHERE requestorId = ? AND statusId <> 8 
+    UNION 
+    SELECT SUM(t.pointCost) AS points 
+    FROM Transactions t 
+        INNER JOIN UserBooks ub on ub.id = t.userBookId 
+    WHERE ub.userId = ? AND statusId <> 8) AS u`
+
+    db.pool.query(outgoing, [userId, userId], (err,results) => {
+        if (err) {
+            throw err
+        }
+            var totalPoints = results[0].totalPoints;
+            //console.log("sql query has returned results: going to send total points to callback: " + totalPoints)
+            return totalPoints;
+    })
+    //console.log("sql query hasn't finished yet ");
+}
 module.exports = {
     isAuthenticated,
+    getPendingPoints,
     transport
 }
