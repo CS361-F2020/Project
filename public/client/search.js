f89c775a4b9e3b385ce91b9c4303ffdbda99d4c4
Glenn Frutiz <glennfrutiz@yahoo.com>
2020-11-18 04:30:18 -0600

    Feature 1: Request a book
    Integrated my features into Andrew's recent pull request.
    *****in public\client\search.js*******
    1. updated requestBook function to prevent requests when the user doesn't have enough points
    
    2. added call to googleapi route created by Melissa to populate book details into a modal
    *****in routes\search.js*******
    Update select all books available using current users location and whether a seller will ship worldwide (all available books from seller same country + books from seller other country that will allow worldwide)
    *****added views\pointsFAQ.handlebars*******
    rudimentary page containing basic rules for points
    *****in views\search.handlebars*******
    1. added link to pointsFAQ
    2. added functionality to "View Details" button provided by Andrew through the inclusion of a modal for book details
    3, prevented user from making request  if they don't have enough points on "Request a copy" and displayed message

--- public/client/search.js
+++ public/client/search.js
@@ -1,11 +1,28 @@
 // request button- activate modal
-function requestBook(bookId, pointCost, title){
+function requestBook(bookId, pointCost, title, userPoints){
     var modalBody = document.getElementById('modalBody');
-    modalBody.innerHTML = `Are you sure you want this book?: <br> <b>"${title}"</b>`
-    var confirmButton = document.getElementById('confirmButton');
-    confirmButton.setAttribute("onclick", `request(${bookId}, ${pointCost}, '${title}')`);
-    // toogle modal
-    $("#pageModal").modal("toggle");
+    if(pointCost > userPoints){
+        modalBody.innerHTML = `<b>You do not have enough points.</b><br>
+        <ul>
+            <li>You earn 1 point when you post a book.</li>
+            <li>You earn points when you respond to a request and ship a book successfully.</li>
+            <ul>
+                <li>2 points are earned if sent to your current country.</li>
+                <li>4 points are earned if sent to another country.</li>  
+            </ul>     
+        </ul>
+        <a href="/search/pointsFAQ" class = "btn btn-info" role = "button">Learn more about the point system</a>`;
+        
+        $("#pageModal").modal("show");
+    }
+    else{
+        
+        modalBody.innerHTML = `Are you sure you want this book?: <br> <b>"${title}"</b>`
+        var confirmButton = document.getElementById('confirmButton');
+        confirmButton.setAttribute("onclick", `request(${bookId}, ${pointCost}, '${title}')`);
+        // toogle modal
+        $("#pageModal").modal("toggle");
+    }
 }
 
 // AJAX request a book
@@ -29,4 +46,42 @@ function request(bookId, pointCost, title){
             alert('Error occured while retrieving book')
         }
     })
+}
+
+function bookDetails(isbn, pointCost, userPoints){
+        $.ajax({
+            url: '/googleapi/isbn/' + isbn,
+            method: 'GET',
+            dataType: 'json',
+            success: function (res) {
+                console.log(res);
+                
+                var details = document.getElementById('details');
+                
+                details.innerHTML = `Title: ${res.title}
+                                    <br>
+                                    Author: ${res.author}
+                                    <br>
+                                    Genre: ${res.genre}
+                                    <br>
+                                    Language: ${res.language} 
+                                    <br>
+                                    Point Cost: ${pointCost}
+                                    <br>
+                                    Rating: ${res.rating}
+                                    <br>
+                                    <a href="${res.googleLink}" class = "btn btn-info" role = "button">Google Link</a>
+                                    <br>
+                                    Description:
+                                    <br>${res.description}`;   
+                $("#detailModal").modal("show");
+                
+            },
+            error: function (jqXHR, textstatus, errorThrown) {
+                console.log(textstatus)
+                alert('Error occured while retrieving book')
+            }
+        })
+    
+    
 }
