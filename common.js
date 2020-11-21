fd0d2a83806885e78bd2e895bd0fca2ac6f9a801
Glenn Frutiz <glennfrutiz@yahoo.com>
2020-11-21 16:54:09 -0600

    debugging some things
    updated common function to get pending points to reflect recent changes to database. removed points FAQ and updated search page to lead to faq instead.

--- common.js
+++ common.js
@@ -39,11 +39,11 @@ function getPoints(userId, callback) {
 function getPendingPoints(userId, callback) {
     var outgoing = `SELECT SUM(u.points) AS totalPoints
     FROM 
-    (SELECT (SUM(Transactions.pointCost) * -1) AS points 
+    (SELECT (SUM(Transactions.buyerPoints) * -1) AS points 
     FROM Transactions 
     WHERE requestorId = ? AND statusId <> 8 
     UNION 
-    SELECT SUM(t.pointCost) AS points 
+    SELECT SUM(t.buyerPoints) AS points 
     FROM Transactions t 
         INNER JOIN UserBooks ub on ub.id = t.userBookId 
     WHERE ub.userId = ? AND statusId <> 8) AS u`
