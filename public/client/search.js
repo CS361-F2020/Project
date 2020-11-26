73f5e1915e5c1984da03a8983f88952f58dfd6d0
Andrew Jung <jungan@flip1.engr.oregonstate.edu>
2020-11-25 20:12:28 -0800

    copies

--- public/client/search.js
+++ public/client/search.js
@@ -1,35 +1,86 @@
 // request button- activate modal
-function requestBook(bookId, pointCost, title, userPoints){
+function requestBook(bookId, pointCost, title, userPoints, condition, date){
+    var condition = condition.split(',');
+    var date = date.split(',');
     var modalBody = document.getElementById('modalBody');
+    var selector = document.getElementsByTagName('select')[0];
+    if(selector != undefined){
+        var div1 = document.getElementById("div1")
+        modalBody.removeChild(div1);
+    }
     if(pointCost > userPoints){
-        
         $("#pointsModal").modal("show");
     }
+    else if(bookId.length > 1){
+        modalBody.innerHTML = `Are you sure you want this book?: <br> <b>"${title}"</b><br>`
+        var div1 = document.createElement("div");
+        div1.id = "div1";
+        div1.setAttribute("class", "form-row")
+        var div2 = document.createElement("div");
+        div2.setAttribute("class", "form-group col-12");
+        var selector = document.createElement("select");
+        selector.id = "selector";
+        selector.setAttribute("class", "form-control");
+        selector.setAttribute("required", "required");
+        selector.setAttribute("onchange", `confirmButton(${pointCost}, '${title}')`);
+        // set a placeholder option
+        var option = document.createElement("option");
+        option.innerHTML="Please Select a Copy";
+        option.setAttribute("disabled", "disabled");
+        option.setAttribute("selected", "selected");
+        selector.appendChild(option);
+        div2.appendChild(selector);
+        div1.appendChild(div2);
+        // set all available options
+        for (var i = 0; i < bookId.length; i++){
+            var option2 = document.createElement("option");
+            option2.value = bookId[i];
+            option2.innerHTML = `Listing date: ${date[i]}, Condition : ${condition[i]}`;
+            selector.appendChild(option2);
+        }
+        var br = document.createElement("br")
+        modalBody.appendChild(br)
+        modalBody.appendChild(div1);
+        // // toogle modal
+        $("#pageModal").modal("toggle");
+    }
     else{
-        
         modalBody.innerHTML = `Are you sure you want this book?: <br> <b>"${title}"</b>`
         var confirmButton = document.getElementById('confirmButton');
-        confirmButton.setAttribute("onclick", `request(${bookId}, ${pointCost}, '${title}')`);
+        confirmButton.setAttribute("onclick", `request(${bookId[0]}, ${pointCost}, '${title}')`);
         // toogle modal
         $("#pageModal").modal("toggle");
     }
 }
 
-// AJAX request a book
-function request(bookId, pointCost, title){
-    console.log(title);
+// Remove confirm button functionality
+function removeOptions(){
+    var confirmButton = document.getElementById('confirmButton');
+    confirmButton.removeAttribute("onclick");
+}
+
+// button is active only if an option is selected
+function confirmButton(pointCost, title){
+    var confirmButton = document.getElementById('confirmButton');
+    confirmButton.setAttribute("onclick", `requestCopies(${pointCost}, '${title}')`);
+}
+
+// AJAX request a book multiple copies avilable
+function requestCopies(pointCost, title){
+    var selected = document.getElementById("selector");
+    var bookId = selected.value;
+    var options = Array.from(selected.children);
+    console.log(options)
     $.ajax({
         url: '/search',
         method: 'POST',
         dataType: 'json',
         data: {'bookId': bookId, 'title': title, 'pointCost': pointCost},
         success: function (res) {
-            // remove the card 
-            // might be better to use flash instead of this so that users get a notification on screen
-            var card = document.getElementById(bookId);
-            card.parentNode.removeChild(card);
+            // reload the load the page to display the correct cards
             // hide the modal
             $('#pageModal').modal('hide');
+            location.reload()
         },
         error: function (jqXHR, textstatus, errorThrown) {
             console.log(textstatus)
@@ -38,31 +89,73 @@ function request(bookId, pointCost, title){
     })
 }
 
-function bookDetails(isbn, pointCost, userPoints){
+// AJAX request a book only one copy avilable
+function request(bookId, pointCost, title){
+    $.ajax({
+        url: '/search',
+        method: 'POST',
+        dataType: 'json',
+        data: {'bookId': bookId, 'title': title, 'pointCost': pointCost},
+        success: function (res) {
+            // remove the card 
+            var card = document.getElementById(bookId);
+            card.parentNode.removeChild(card);
+            // hide the modal
+            $('#pageModal').modal('hide');
+            location.reload();
+        },
+        error: function (jqXHR, textstatus, errorThrown) {
+            console.log(textstatus)
+            alert('Error occured while retrieving book')
+        }
+    })
+}
+
+function bookDetails(isbn, pointCost){
         $.ajax({
             url: '/googleapi/isbn/' + isbn,
             method: 'GET',
             dataType: 'json',
             success: function (res) {
-                console.log(res);
-                
-                var details = document.getElementById('details');
                 $("#detailTitle").text(res.title)
                 $("#detailAuthor").text(res.author)
                 $("#detailGenre").text(res.genre)
                 $("#detailLanguage").text(res.language)
                 $("#detailPointCost").text(pointCost)
                 $("#detailRating").text(res.rating)
-                //need to turn this into a button
-                //$("#detailGoogleLink").text(res.googleLink)
                 $("#detailDescription").text(res.description)
-               
                 $("#detailModal").modal("show");
-                
             },
             error: function (jqXHR, textstatus, errorThrown) {
                 console.log(textstatus)
                 alert('Error occured while retrieving book')
             }
         })  
+}
+
+function searchResults() {
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+    
+    for (i = 0; i < cards.length; i++) {
+        genre = cards[i].querySelector(".card-body div.card-genre");
+        author = cards[i].querySelector(".card-body p.card-author");
+        title = cards[i].querySelector(".card-body p.card-title");
+        rating = cards[i].querySelector(".card-body div.card-rating");
+        pubDate = cards[i].querySelector(".card-body div.card-pubdate");
+
+        if ((genre.innerText.toUpperCase().indexOf(filter)   > -1) ||
+            (author.innerText.toUpperCase().indexOf(filter)  > -1) ||
+            (title.innerText.toUpperCase().indexOf(filter)   > -1) ||
+            (rating.innerText.toUpperCase().indexOf(filter)  > -1) ||
+            (pubDate.innerText.toUpperCase().indexOf(filter) > -1)) 
+        {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
 }
--- routes/search.js
+++ routes/search.js
@@ -11,8 +11,24 @@ function Book(data) {
     this.imgUrl = data.imgUrl;
     this.pointcost = data.pointcost;
     this.isbn = data.isbn;
+    this.genre = data.genre;
+    this.rating = data.rating;
+    this.pubDate = data.pubDate;
     this.userPoints = data.userPoints;
+    this.condition = data.condition;
+    this.date = data.date;
+    this.copies = data.copies;
 }
+
+//Glenn: I was testing out character removal with this function. 
+//I think it works a little better than the one from the stack overflow post provided the other day
+//maybe a good candidate for handling characters?
+function replaceCharacters(userText) {
+    var regExpr = /[^a-zA-Z0-9-. ]/g;
+    
+    return userText.replace(regExpr, "");
+}
+
         
 // @route   GET /allBooks
 // @desc    Get all available books that don't belong to the user and are not undergoing transactions
@@ -26,17 +42,20 @@ router.get('/', common.isAuthenticated, (req, res, next) => {
 // Get all avaialable books
 // ******** need to update this based on worldwide shippers
 function allBooks(id, callback){
-    var selectAllAvailableBooks = `SELECT UserBooks.id AS id, title AS title, author AS author, imgUrl AS imgUrl, isbn13 AS isbn, Users.country
-                                 FROM UserBooks
-                                 INNER JOIN Books ON Books.id = UserBooks.bookId
-                                 INNER JOIN Users ON Users.id = UserBooks.userId
-                                 WHERE UserBooks.userId != ? AND UserBooks.available = 1 AND Users.worldwide = 1
-                                 UNION
-                                 SELECT UserBooks.id AS id, title AS title, author AS author, imgUrl AS imgUrl, isbn13 AS isbn, Users.country
-                                 FROM UserBooks
-                                 INNER JOIN Books ON Books.id = UserBooks.bookId
-                                 INNER JOIN Users ON Users.id = UserBooks.userId
-                                 WHERE UserBooks.userId != ? AND UserBooks.available = 1 AND Users.country = ?`;
+    var selectAllAvailableBooks = `SELECT UserBooks.id AS id, listingDate, description, title AS title, author AS author, imgUrl AS imgUrl, isbn13 AS isbn, Users.country
+                                FROM UserBooks
+                                INNER JOIN Conditions ON Conditions.id = UserBooks.conditionId
+                                INNER JOIN Books ON Books.id = UserBooks.bookId
+                                INNER JOIN Users ON Users.id = UserBooks.userId
+                                WHERE UserBooks.userId != 1 AND UserBooks.available = 1 AND Users.worldwide = 1
+                                UNION
+                                SELECT UserBooks.id AS id, listingDate, description, title AS title, author AS author, imgUrl AS imgUrl, isbn13 AS isbn, Users.country
+                                FROM UserBooks
+                                INNER JOIN Conditions ON Conditions.id = UserBooks.conditionId
+                                INNER JOIN Books ON Books.id = UserBooks.bookId
+                                INNER JOIN Users ON Users.id = UserBooks.userId
+                                WHERE UserBooks.userId != 1 AND UserBooks.available = 1 AND Users.country = 1
+                                ORDER BY isbn ASC`;
 
     var selectUserCountry = `SELECT Users.country AS country, Users.points AS userPoints
                             FROM Users
@@ -64,22 +83,44 @@ function allBooks(id, callback){
             var number = result.length;
             // ********* need to update the point value
             var points; 
+            var copy;
+            var count = 0;
             for (let i = 0; i < number; i++) {
                 points = 2;
                 if(country != result[i].country){
                     points = 4;
                 }
                 var data = {
-                    id: result[i].id,
+                    id: [result[i].id],
                     title: result[i].title,
                     author: result[i].author,
                     imgUrl: result[i].imgUrl,
                     pointcost: points,
                     isbn: result[i].isbn,
-                    userPoints: userPoints
+                    userPoints: userPoints,
+                    condition: [result[i].description],
+                    date: [result[i].listingDate],
+                    genre: result[i].genre,
+                    rating: result[i].rating,
+                    pubDate: result[i].pubDate,
+                    copies: 0
                 }
-                payload.books.push(new Book(data));           
-            }     
+                if (i != 0 && copy.isbn == data.isbn){
+                    if (count == 0){
+                        copy.copies++;
+                    }
+                    copy.id.push(result[i].id);
+                    copy.condition.push(result[i].description);
+                    copy.date.push(result[i].listingDate);
+                    copy.copies++;
+                    count++;
+                    continue;
+                }
+                payload.books.push(new Book(data));   
+                copy = payload.books[payload.books.length-1];  
+                count = 0;      
+            }  
+            console.log(payload)  
             return callback(payload);
         })
     })
@@ -124,6 +165,7 @@ router.post('/', common.isAuthenticated, (req, res, next) => {
                                 res.render('search', payload);
                             })
                         }
+                        var successMsg = req.body.title + ' has been requested!';
                         // database has been updated
                         // send an email to the seller that a book has been requested
                         // **** update text or maybe use html
@@ -133,13 +175,14 @@ router.post('/', common.isAuthenticated, (req, res, next) => {
                             text: 'You have a new request for ' + title
                         }
                         common.transport.sendMail(message);
-                
-                        res.send({success: true});
+                        req.flash('success', successMsg)
+                        res.send({success: 'success'});
                     });
                 }
             });
         }
     });
 });
+
 
 module.exports = router;
